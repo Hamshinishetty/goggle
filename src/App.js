dd35d32a5a4a38638154db166cee4a04fae73f3e
Hamshini <hamshini11203@gmail.com>
2024-06-21 18:48:10 +0530

    ahhhhh

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
-import Routes from './components/Routes';
+import Routess from './components/Routess';
 import Footer from './components/Footer';
+import Search from './components/Search';
 
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
 
   return (
     <div className={darkTheme ? 'dark':'' }>
-      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen text-1xl">
-        <Navbar />
-        <Routes />
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-100 min-h-screen text-3xl">
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+        <Routess />
         <Footer />
+        <Search />
       </div>
     </div>
   );
---
+++ src/components/Routess.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+const Routess = () => {
+  return (
+    <div className = 'p-4'>
+        <h1>
+          <Routes>
+            <Route>
+
+            </Route>
+          </Routes>
+        </h1>
+    </div>
+  );
+}
+
+export default Routess;
